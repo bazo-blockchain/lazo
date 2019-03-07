23d6a5341f9d755eea7792b034fae79a27baac85
Keerthikan@Asus <kk.devpro11@gmail.com>
2019-03-07 16:58:46 +0100

    Test relational comparison operators (<, <=, >, >=)

--- parser/parser_expression_test.go
+++ parser/parser_expression_test.go
@@ -7,9 +7,10 @@ import (
 )
 
 // Binary Expressions
-// ------------------------
+// ==============================
 
 // Logic operators
+// ---------------
 
 func TestLogicOr(t *testing.T) {
 	e := parseExpressionFromInput(t, "x || y || z")
@@ -32,6 +33,7 @@ func TestMixedLogicOperatorOrAnd(t *testing.T) {
 }
 
 // Equality operators
+// ------------------
 
 func TestEquality(t *testing.T) {
 	e := parseExpressionFromInput(t, "5 == 4")
@@ -51,6 +53,34 @@ func TestEqualityPrecedence(t *testing.T) {
 func TestEqualityAssociativity(t *testing.T) {
 	e := parseExpressionFromInput(t, "x == y == true")
 	node.AssertBinaryExpression(t, e, "(x == y)", "true", token.Equal)
+}
+
+// Relational operators
+// --------------------
+
+func TestLess(t *testing.T) {
+	e := parseExpressionFromInput(t, "1 < 3")
+	node.AssertBinaryExpression(t, e, "1", "3", token.Less)
+}
+
+func TestLessEqual(t *testing.T) {
+	e := parseExpressionFromInput(t, "1 <= 3")
+	node.AssertBinaryExpression(t, e, "1", "3", token.LessEqual)
+}
+
+func TestGreater(t *testing.T) {
+	e := parseExpressionFromInput(t, "1 > 3")
+	node.AssertBinaryExpression(t, e, "1", "3", token.Greater)
+}
+
+func TestGreaterEqual(t *testing.T) {
+	e := parseExpressionFromInput(t, "1 >= 3")
+	node.AssertBinaryExpression(t, e, "1", "3", token.GreaterEqual)
+}
+
+func TestRelationalComparisonAssociativity(t *testing.T) {
+	e := parseExpressionFromInput(t, "1 < 2 <= 3 > 4 >= 5")
+	node.AssertBinaryExpression(t, e, "(((1 < 2) <= 3) > 4)", "5", token.GreaterEqual)
 }
 
 // --------------
