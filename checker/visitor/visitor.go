113bf56c76242239c61edc72c68fd1c4dac760cb
Remo Pfister <remo.pfister@hsr.ch>
2019-03-08 09:37:53 +0100

    Implement visitor interface

---
+++ checker/visitor/visitor.go
@@ -0,0 +1,23 @@
+package visitor
+
+import "github.com/bazo-blockchain/lazo/parser/node"
+
+type Visitor interface {
+	VisitProgramNode(node *node.ProgramNode)
+	VisitContractNode(node *node.ContractNode)
+	VisitFunctionNode(node *node.FunctionNode)
+	VisitVariableNode(node *node.VariableNode)
+	VisitTypeNode(node *node.TypeNode)
+	VisitIfStatementNode(node *node.IfStatementNode)
+	VisitReturnStatementNode(node *node.ReturnStatementNode)
+	VisitStatementBlockNode(node *node.StatementBlockNode)
+	VisitAssignmentStatementNode(node *node.AssignmentStatementNode)
+	VisitBinaryExpressionNode(node *node.BinaryExpressionNode)
+	VisitUnaryExpressionNode(node *node.UnaryExpression)
+	VisitDesignatorNode(node *node.DesignatorNode)
+	VisitIntegerLiteralNode(node *node.IntegerLiteralNode)
+	VisitStringLiteralNode(node *node.StringLiteralNode)
+	VisitCharacterLiteralNode(node *node.CharacterLiteralNode)
+	VisitBoolLiteralNode(node *node.BoolLiteralNode)
+	VisitErrorNode(node *node.ErrorNode)
+}
