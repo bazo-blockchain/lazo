6bb04011d941aa8c632d345e0beca5e1026210e4
Remo Pfister <remo.pfister@hsr.ch>
2019-03-08 10:58:49 +0100

    Moved interface to avoid import cycle

--- checker/visitor/visitor.go
+++ checker/visitor/visitor.go
@@ -2,42 +2,57 @@ package visitor
 
 import "github.com/bazo-blockchain/lazo/parser/node"
 
-type Visitor interface {
-	VisitProgramNode(node *node.ProgramNode)
-	VisitContractNode(node *node.ContractNode)
-	VisitFunctionNode(node *node.FunctionNode)
-	VisitVariableNode(node *node.VariableNode)
-	VisitTypeNode(node *node.TypeNode)
-	VisitIfStatementNode(node *node.IfStatementNode)
-	VisitReturnStatementNode(node *node.ReturnStatementNode)
-	VisitAssignmentStatementNode(node *node.AssignmentStatementNode)
-	VisitBinaryExpressionNode(node *node.BinaryExpressionNode)
-	VisitUnaryExpressionNode(node *node.UnaryExpression)
-	VisitDesignatorNode(node *node.DesignatorNode)
-	VisitIntegerLiteralNode(node *node.IntegerLiteralNode)
-	VisitStringLiteralNode(node *node.StringLiteralNode)
-	VisitCharacterLiteralNode(node *node.CharacterLiteralNode)
-	VisitBoolLiteralNode(node *node.BoolLiteralNode)
-	VisitErrorNode(node *node.ErrorNode)
-}
+
 
 type BaseVisitor struct {}
 
-func (v *BaseVisitor) VisitProgramNode(node *node.ProgramNode) {}
-func (v *BaseVisitor) VisitContractNode(node *node.ContractNode) {}
-func (v *BaseVisitor) VisitFunctionNode(node *node.FunctionNode) {}
-func (v *BaseVisitor) VisitVariableNode(node *node.VariableNode) {}
-func (v *BaseVisitor) VisitTypeNode(node *node.TypeNode) {}
-func (v *BaseVisitor) VisitIfStatementNode(node *node.IfStatementNode) {}
-func (v *BaseVisitor) VisitReturnStatementNode(node *node.ReturnStatementNode) {}
-func (v *BaseVisitor) VisitAssignmentStatementNode(node *node.AssignmentStatementNode) {}
-func (v *BaseVisitor) VisitBinaryExpressionNode(node *node.BinaryExpressionNode) {}
-func (v *BaseVisitor) VisitUnaryExpressionNode(node *node.UnaryExpression) {}
-func (v *BaseVisitor) VisitDesignatorNode(node *node.DesignatorNode) {}
-func (v *BaseVisitor) VisitIntegerLiteralNode(node *node.IntegerLiteralNode) {}
-func (v *BaseVisitor) VisitStringLiteralNode(node *node.StringLiteralNode) {}
-func (v *BaseVisitor) VisitCharacterLiteralNode(node *node.CharacterLiteralNode) {}
-func (v *BaseVisitor) VisitBoolLiteralNode(node *node.BoolLiteralNode) {}
-func (v *BaseVisitor) VisitErrorNode(node *node.ErrorNode) {}
+func (v *BaseVisitor) VisitProgramNode(node *node.ProgramNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitContractNode(node *node.ContractNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitFunctionNode(node *node.FunctionNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitVariableNode(node *node.VariableNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitTypeNode(node *node.TypeNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitIfStatementNode(node *node.IfStatementNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitReturnStatementNode(node *node.ReturnStatementNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitAssignmentStatementNode(node *node.AssignmentStatementNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitBinaryExpressionNode(node *node.BinaryExpressionNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitUnaryExpressionNode(node *node.UnaryExpression) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitDesignatorNode(node *node.DesignatorNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitIntegerLiteralNode(node *node.IntegerLiteralNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitStringLiteralNode(node *node.StringLiteralNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitCharacterLiteralNode(node *node.CharacterLiteralNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitBoolLiteralNode(node *node.BoolLiteralNode) {
+	// TODO Implement
+}
+func (v *BaseVisitor) VisitErrorNode(node *node.ErrorNode) {
+	// TODO Implement
+}
 
 
--- parser/node/nodes.go
+++ parser/node/nodes.go
@@ -2,7 +2,6 @@ package node
 
 import (
 	"fmt"
-	"github.com/bazo-blockchain/lazo/checker/visitor"
 	"github.com/bazo-blockchain/lazo/lexer/token"
 	"math/big"
 )
@@ -10,7 +9,7 @@ import (
 type Node interface {
 	Pos() token.Position
 	String() string
-	Accept(v visitor.Visitor)
+	Accept(v Visitor)
 }
 
 type AbstractNode struct {
@@ -41,7 +40,7 @@ func (n *ProgramNode) String() string {
 	return fmt.Sprintf("%s", n.Contract)
 }
 
-func (n *ProgramNode) Accept(v visitor.Visitor) {
+func (n *ProgramNode) Accept(v Visitor) {
 	v.VisitProgramNode(n)
 }
 
@@ -58,7 +57,7 @@ func (n *ContractNode) String() string {
 	return fmt.Sprintf("[%s] CONTRACT %s \n VARS: %s \n\n FUNCS: %s", n.Pos(), n.Name, n.Variables, n.Functions)
 }
 
-func (n *ContractNode) Accept(v visitor.Visitor) {
+func (n *ContractNode) Accept(v Visitor) {
 	v.VisitContractNode(n)
 }
 
@@ -79,7 +78,7 @@ func (n *FunctionNode) String() string {
 		n.Pos(), n.Name, n.Parameters, n.ReturnTypes, n.Body)
 }
 
-func (n *FunctionNode) Accept(v visitor.Visitor) {
+func (n *FunctionNode) Accept(v Visitor) {
 	v.VisitFunctionNode(n)
 }
 
@@ -98,7 +97,7 @@ func (n *VariableNode) String() string {
 	return fmt.Sprintf("\n [%s] VARIABLE %s %s = %s", n.Pos(), n.Type.Identifier, n.Identifier, n.Expression)
 }
 
-func (n *VariableNode) Accept(v visitor.Visitor) {
+func (n *VariableNode) Accept(v Visitor) {
 	v.VisitVariableNode(n)
 }
 
@@ -113,7 +112,7 @@ func (n *TypeNode) String() string {
 	return fmt.Sprintf("TYPE %s", n.Identifier)
 }
 
-func (n *TypeNode) Accept(v visitor.Visitor) {
+func (n *TypeNode) Accept(v Visitor) {
 	v.VisitTypeNode(n)
 }
 
@@ -130,7 +129,7 @@ func (n *IfStatementNode) String() string {
 	return fmt.Sprintf("\n [%s] IF %s THEN %s ELSE %s", n.Pos(), n.Condition, n.Then, n.Else)
 }
 
-func (n *IfStatementNode) Accept(v visitor.Visitor) {
+func (n *IfStatementNode) Accept(v Visitor) {
 	v.VisitIfStatementNode(n)
 }
 
@@ -145,8 +144,8 @@ func (n *ReturnStatementNode) String() string {
 	return fmt.Sprintf("\n [%s] RETURNSTMT %s", n.Pos(), n.Expression)
 }
 
-func (n *ReturnStatementNode) Accept(v visitor.Visitor) {
-	v.VisitAssignmentStatementNode(n)
+func (n *ReturnStatementNode) Accept(v Visitor) {
+	v.VisitReturnStatementNode(n)
 }
 
 // --------------------------
@@ -161,7 +160,7 @@ func (n *AssignmentStatementNode) String() string {
 	return fmt.Sprintf("\n [%s] ASSIGN %s %s", n.Pos(), n.Left, n.Right)
 }
 
-func (n *AssignmentStatementNode) Accept(v visitor.Visitor) {
+func (n *AssignmentStatementNode) Accept(v Visitor) {
 	v.VisitAssignmentStatementNode(n)
 }
 
@@ -180,7 +179,7 @@ func (n *BinaryExpressionNode) String() string {
 	return fmt.Sprintf("(%s %s %s)", n.Left, token.SymbolLexeme[n.Operator], n.Right)
 }
 
-func (n *BinaryExpressionNode) Accept(v visitor.Visitor) {
+func (n *BinaryExpressionNode) Accept(v Visitor) {
 	v.VisitBinaryExpressionNode(n)
 }
 
@@ -196,7 +195,7 @@ func (n *UnaryExpression) String() string {
 	return fmt.Sprintf("EXPR (%s %s)", token.SymbolLexeme[n.Operator], n.Operand)
 }
 
-func (n *UnaryExpression) Accept(v visitor.Visitor) {
+func (n *UnaryExpression) Accept(v Visitor) {
 	v.VisitUnaryExpressionNode(n)
 }
 
@@ -211,7 +210,7 @@ func (n *DesignatorNode) String() string {
 	return fmt.Sprintf("%s", n.Value)
 }
 
-func (n *DesignatorNode) Accept(v visitor.Visitor) {
+func (n *DesignatorNode) Accept(v Visitor) {
 	v.VisitDesignatorNode(n)
 }
 
@@ -228,7 +227,7 @@ func (n *IntegerLiteralNode) String() string {
 	return fmt.Sprintf("%d", n.Value)
 }
 
-func (n *IntegerLiteralNode) Accept(v visitor.Visitor) {
+func (n *IntegerLiteralNode) Accept(v Visitor) {
 	v.VisitIntegerLiteralNode(n)
 }
 
@@ -243,7 +242,7 @@ func (n *StringLiteralNode) String() string {
 	return fmt.Sprintf("%s", n.Value)
 }
 
-func (n *StringLiteralNode) Accept(v visitor.Visitor) {
+func (n *StringLiteralNode) Accept(v Visitor) {
 	v.VisitStringLiteralNode(n)
 }
 
@@ -258,7 +257,7 @@ func (n *CharacterLiteralNode) String() string {
 	return fmt.Sprintf("%c", n.Value)
 }
 
-func (n *CharacterLiteralNode) Accept(v visitor.Visitor) {
+func (n *CharacterLiteralNode) Accept(v Visitor) {
 	v.VisitCharacterLiteralNode(n)
 }
 
@@ -273,7 +272,7 @@ func (n *BoolLiteralNode) String() string {
 	return fmt.Sprintf("%t", n.Value)
 }
 
-func (n *BoolLiteralNode) Accept(v visitor.Visitor) {
+func (n *BoolLiteralNode) Accept(v Visitor) {
 	v.VisitBoolLiteralNode(n)
 }
 
@@ -288,6 +287,26 @@ func (n *ErrorNode) String() string {
 	return fmt.Sprintf("[%s] ERROR: %s", n.Pos(), n.Message)
 }
 
-func (n *ErrorNode) Accept(v visitor.Visitor) {
+func (n *ErrorNode) Accept(v Visitor) {
 	v.VisitErrorNode(n)
+}
+
+// NOTE: This interface is placed here to avoid import cycle
+type Visitor interface {
+	VisitProgramNode(node *ProgramNode)
+	VisitContractNode(node *ContractNode)
+	VisitFunctionNode(node *FunctionNode)
+	VisitVariableNode(node *VariableNode)
+	VisitTypeNode(node *TypeNode)
+	VisitIfStatementNode(node *IfStatementNode)
+	VisitReturnStatementNode(node *ReturnStatementNode)
+	VisitAssignmentStatementNode(node *AssignmentStatementNode)
+	VisitBinaryExpressionNode(node *BinaryExpressionNode)
+	VisitUnaryExpressionNode(node *UnaryExpression)
+	VisitDesignatorNode(node *DesignatorNode)
+	VisitIntegerLiteralNode(node *IntegerLiteralNode)
+	VisitStringLiteralNode(node *StringLiteralNode)
+	VisitCharacterLiteralNode(node *CharacterLiteralNode)
+	VisitBoolLiteralNode(node *BoolLiteralNode)
+	VisitErrorNode(node *ErrorNode)
 }
