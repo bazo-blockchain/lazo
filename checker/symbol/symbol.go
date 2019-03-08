6084dde9bd2a125f8139c0d2fdfc329a8a97f1df
Remo Pfister <remo.pfister@hsr.ch>
2019-03-08 09:54:45 +0100

    Implement BaseSymbol and Symbol interface

--- checker/symbol/symbol.go
+++ checker/symbol/symbol.go
@@ -1,4 +1,29 @@
 package symbol
 
-// TODO Implement Symbols
-type Symbol interface {}
+type Symbol interface {
+	NewSymbol() Symbol
+	AllDeclarations() []Symbol
+	String() string
+}
+
+type BaseSymbol struct {
+	Scope Symbol
+	Identifier string
+}
+
+func (sym *BaseSymbol) NewSymbol(scope Symbol, identifier string) BaseSymbol {
+	return BaseSymbol{
+		Scope: scope,
+		Identifier: identifier,
+	}
+}
+
+func (sym *BaseSymbol) AllDeclarations() []BaseSymbol {
+	return []BaseSymbol{}
+}
+
+func (sym *BaseSymbol) String() string {
+	return sym.Identifier
+}
+
+// TODO Implement further symbols
