f3723b329738bd19fea5f850147e7907abf3c7f3
Keerthikan@Asus <kk.devpro11@gmail.com>
2019-03-09 15:34:44 +0100

    Add flag for compilation stage
    The flag --stage or -s helps to control the compilation stage.
    Example: --stage=p compiles only until parser

--- cmd/compile.go
+++ cmd/compile.go
@@ -4,19 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/bazo-blockchain/lazo/checker"
+	"github.com/bazo-blockchain/lazo/checker/symbol"
 	"github.com/bazo-blockchain/lazo/lexer"
 	"github.com/bazo-blockchain/lazo/parser"
+	"github.com/bazo-blockchain/lazo/parser/node"
 	"github.com/spf13/cobra"
+	"io"
 	"os"
 )
 
+var stage string
+
 func init() {
 	rootCmd.AddCommand(compileCommand)
+
+	compileCommand.Flags().StringVarP(
+		&stage,
+		"stage",
+		"s",
+		"c",
+		"Compilation stage. \nAvailable stages: l=lexer, p=parser, c=checker")
 }
 
 var compileCommand = &cobra.Command{
-	Use:   "compile",
-	Short: "Compile the Lazo source code",
+	Use:     "compile [source file]",
+	Short:   "Compile the Lazo source code",
+	Example: "  lazo compile program.lazo --stage=l",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
 			cmd.Help()
@@ -28,30 +41,49 @@ var compileCommand = &cobra.Command{
 
 func compile(sourceFile string) {
 	file, err := os.Open(sourceFile)
-	check(err)
+	if err != nil {
+		panic(err)
+	}
 
+	lexer := scan(file)
+	syntaxTree := parse(lexer)
+	_ = check(syntaxTree)
+}
+
+func scan(file io.Reader) *lexer.Lexer {
 	lexer := lexer.New(bufio.NewReader(file))
-	//for !lexer.IsEnd {
-	//	tok := lexer.NextToken()
-	//	fmt.Printf("%s \n", tok)
-	//}
+	if stage == "l" {
+		for !lexer.IsEnd {
+			tok := lexer.NextToken()
+			fmt.Printf("%s \n", tok)
+		}
+		os.Exit(0)
+	}
+	return lexer
+}
 
-	parser := parser.New(lexer)
+func parse(l *lexer.Lexer) *node.ProgramNode {
+	parser := parser.New(l)
 	syntaxTree, errors := parser.ParseProgram()
 	if len(errors) > 0 {
 		fmt.Fprintln(os.Stderr, errors)
+		fmt.Println(syntaxTree)
+		os.Exit(1)
 	}
+	if stage == "p" {
+		fmt.Println(syntaxTree)
+		os.Exit(0)
+	}
+	return syntaxTree
+}
 
+func check(syntaxTree *node.ProgramNode) *symbol.Table {
 	checker := checker.New(syntaxTree)
 	symbolTable, errors := checker.Run()
 	if len(errors) > 0 {
 		fmt.Fprintln(os.Stderr, errors)
+		fmt.Println(symbolTable)
+		os.Exit(1)
 	}
-	fmt.Println(symbolTable)
-}
-
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
+	return symbolTable
 }
