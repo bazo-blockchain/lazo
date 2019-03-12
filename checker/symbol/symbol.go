0523df3e1ada96ce689299b7b7c462260c993f7c
Remo Pfister <remo.pfister@hsr.ch>
2019-03-12 11:30:22 +0100

    Implement CompilationUnit and other symbols

--- checker/symbol/symbol.go
+++ checker/symbol/symbol.go
@@ -1,7 +1,7 @@
 package symbol
 
 type Symbol interface {
-	NewSymbol() Symbol
+	NewSymbol(scope Symbol, identifier string) Symbol
 	AllDeclarations() []Symbol
 	String() string
 }
@@ -11,19 +11,179 @@ type BaseSymbol struct {
 	Identifier string
 }
 
-func (sym *BaseSymbol) NewSymbol(scope Symbol, identifier string) BaseSymbol {
-	return BaseSymbol{
+func (sym *BaseSymbol) NewSymbol(scope Symbol, identifier string) Symbol {
+	return &BaseSymbol{
 		Scope: scope,
 		Identifier: identifier,
 	}
 }
 
-func (sym *BaseSymbol) AllDeclarations() []BaseSymbol {
-	return []BaseSymbol{}
+func (sym *BaseSymbol) AllDeclarations() []Symbol {
+	return []Symbol{}
 }
 
 func (sym *BaseSymbol) String() string {
 	return sym.Identifier
 }
 
-// TODO Implement further symbols
+// Concrete Symbols
+//----------------
+
+type CompilationUnit struct {
+	Symbol
+	Contract *ContractSymbol
+	Types *[]TypeSymbol
+	Functions *[]FunctionSymbol
+	Constants *[]ConstantSymbol
+
+	NullType *TypeSymbol
+	BoolType *TypeSymbol
+	CharType *TypeSymbol
+	StringType *TypeSymbol
+	IntType *TypeSymbol
+
+	BuiltInTypes *[]TypeSymbol
+
+	TrueConstant *ConstantSymbol
+	FalseConstant *ConstantSymbol
+	NullConstant *ConstantSymbol
+}
+
+func (cu *CompilationUnit) NewCompilationUnit() Symbol{
+	return &CompilationUnit{
+		Symbol: BaseSymbol{}.NewSymbol(nil, ""),
+		NullType: TypeSymbol{}.NewTypeSymbol(cu, "@NULL"),
+	}
+}
+
+func (cu *CompilationUnit) AllDeclarations() []Symbol {
+	// TODO implement
+	return nil
+}
+
+//----------------
+
+type ContractSymbol struct {
+	// TODO Implement
+}
+
+func (sym *ContractSymbol) NewSymbol(scope Symbol, identifier string) Symbol {
+	// TODO Implement
+	return nil
+}
+
+//----------------
+
+type FieldSymbol struct {
+	// TODO Implement
+}
+
+func (sym *FieldSymbol) NewSymbol(scope Symbol, identifier string) Symbol {
+	// TODO Implement
+	return nil
+}
+
+//----------------
+
+type FunctionSymbol struct {
+	// TODO Implement
+}
+
+func (sym *FunctionSymbol) NewSymbol(scope Symbol, identifier string) Symbol {
+	// TODO Implement
+	return nil
+}
+
+//----------------
+
+type ParameterSymbol struct {
+	// TODO Implement
+}
+
+func (sym *ParameterSymbol) NewSymbol(scope Symbol, identifier string) Symbol {
+	// TODO Implement
+	return nil
+}
+
+//----------------
+
+type LocalVariableSymbol struct {
+	// TODO Implement
+}
+
+func (sym *LocalVariableSymbol) NewSymbol(scope Symbol, identifier string) Symbol {
+	// TODO Implement
+	return nil
+}
+
+//----------------
+
+type VariableSymbol struct {
+	Symbol
+	Type *TypeSymbol
+}
+
+func (sym *VariableSymbol) NewSymbol(scope Symbol, identifier string) Symbol {
+	if scope == nil {
+		// TODO Error
+	}
+	return &VariableSymbol{
+		Symbol: BaseSymbol{}.NewSymbol(scope, identifier),
+	}
+}
+
+//----------------
+
+type ConstantSymbol struct {
+	Symbol
+	Type *TypeSymbol
+}
+
+func (sym *ConstantSymbol) NewSymbol(scope Symbol, identifier string) Symbol {
+	if scope == nil {
+		// TODO Error
+	}
+	if identifier == "" {
+		// TODO Error
+	}
+
+	return &ConstantSymbol{
+		Symbol: BaseSymbol{}.NewSymbol(scope, identifier),
+	}
+}
+
+func (sym *ConstantSymbol) NewConstantSymbol(scope Symbol, identifier string, typeSymbol *TypeSymbol) *ConstantSymbol {
+	constantSymbol := sym.NewSymbol(scope, identifier)
+	if typeSymbol == nil {
+		// TODO Error
+	}
+	symbol, _ := constantSymbol.(*ConstantSymbol)
+	symbol.Type = typeSymbol
+	return symbol
+}
+
+//----------------
+
+type TypeSymbol struct {
+	Symbol
+}
+
+func (sym *TypeSymbol) NewSymbol(scope Symbol, identifier string) Symbol {
+	if scope == nil {
+		// TODO Error
+	}
+	if identifier == "" {
+		// TODO Error
+	}
+	return &TypeSymbol{
+		Symbol: BaseSymbol{}.NewSymbol(scope, identifier),
+	}
+}
+
+func (sym *TypeSymbol) NewTypeSymbol(scope Symbol, identifier string) *TypeSymbol {
+	baseSymbol := sym.NewSymbol(scope, identifier)
+	symbol, _ := baseSymbol.(*TypeSymbol)
+	return symbol
+}
+
+//----------------
