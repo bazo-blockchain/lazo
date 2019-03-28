a31630b33b8b71cf8285a9832c27a4fa9449a8a7
Keerthikan@Asus <kk.devpro11@gmail.com>
2019-03-28 01:21:16 +0100

    Call the first function automatically

--- cmd/run.go
+++ cmd/run.go
@@ -29,10 +29,10 @@ func execute(sourceFile string) {
 	context.ContractVariables = variables
 	context.Fee += (uint64(len(variables))) * 1000
 	context.Data = []byte{
-		// 0, // total bytes
-		// 0, // Contract Init Flag
-		3,
-		19, 70, 101, 78, // Function hash
+		0, // total bytes
+		0, // Contract Init Flag
+		//3,
+		//19, 70, 101, 78, // Function hash
 	}
 
 	vm := vm.NewVM(context)
