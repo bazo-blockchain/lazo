310745d9913a6c4a53cbeb6ae8cad129df5fc70e
Keerthikan@Asus <kk.devpro11@gmail.com>
2019-03-28 12:00:58 +0100

    Test function hash of function signature

--- generator/emit/il_builder_test.go
+++ generator/emit/il_builder_test.go
@@ -66,19 +66,40 @@ func TestFuncSig(t *testing.T) {
 }
 
 func TestFuncHashes(t *testing.T) {
+	// sha256 values are generated using this tool: https://passwordsgenerator.net/sha256-hash-generator/
 	testData := map[string][]byte{
-		"()test()":    {0xD1, 0xFC, 0x69, 0xEB},
-		"(int)test()": {0x13, 0x46, 0x65, 0x4E},
-		//"(int,bool)test()",
-		//"()test(int)",
-		//"()test(int,int)",
-		//"(int,bool)test(int,bool)"
+		"()test()":                 {0xD1, 0xFC, 0x69, 0xEB},
+		"(int)test()":              {0x13, 0x46, 0x65, 0x4E},
+		"(int,bool)test()":         {0xC0, 0x26, 0x4C, 0xF0},
+		"()test(int)":              {0x21, 0xB0, 0x59, 0xA3},
+		"()test(int,int)":          {0x10, 0xD3, 0x28, 0x4F},
+		"(int,bool)test(int,bool)": {0x77, 0xA1, 0x94, 0x20},
 	}
 
 	for k, v := range testData {
 		hash := createFuncHash(k)
 		for i, h := range hash {
-			assert.Equal(t, h, v[i])
+			assert.Equal(t, h, v[i], k)
 		}
+	}
+}
+
+func TestFuncHashWithSymbol(t *testing.T) {
+	f := symbol.NewFunctionSymbol(nil, "test")
+	intType := symbol.NewTypeSymbol(f, "int")
+	boolType := symbol.NewTypeSymbol(f, "bool")
+	f.ReturnTypes = append(f.ReturnTypes, intType)
+	f.ReturnTypes = append(f.ReturnTypes, boolType)
+
+	p1 := symbol.NewParameterSymbol(nil, "x")
+	p2 := symbol.NewParameterSymbol(nil, "y")
+	p1.Type = intType
+	p2.Type = boolType
+	f.Parameters = append(f.Parameters, p1, p2)
+
+	actual := createFuncHash(createFuncSignature(f))
+	expected := []byte{0x77, 0xA1, 0x94, 0x20}
+	for i, h := range actual {
+		assert.Equal(t, h, expected[i])
 	}
 }
